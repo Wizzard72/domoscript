42f3aa13030c1b93ed563999c7f0aa103d554ea9
Wizzard72 <paulslats@gmail.com>
2017-02-18 21:50:12 +0100

    Update script_device_door.lua

--- script_device_door.lua
+++ script_device_door.lua
@@ -3,7 +3,7 @@
     /home/domoticz/domoticz/scripts/lua/script_device_door_sensor_v1.0.lua
 	
     -- Autors  ----------------------------------------------------------------
-    V1.0 - Paul Slats
+    V1.0 - Wizzard72
 	
 	
     -- Uservariables ----------------------------------------------------------
