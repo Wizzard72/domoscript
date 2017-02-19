65408bc3eef0e40c15f7895ac57c28f98cf87421
Wizzard72 <paulslats@gmail.com>
2017-02-19 18:41:43 +0100

    Update script_time_livingroom_v1.0.lua

--- script_time_livingroom_v1.0.lua
+++ script_time_livingroom_v1.0.lua
@@ -3,7 +3,7 @@
     /home/domoticz/domoticz/scripts/lua/script_time_livingroom_v1.0.lua
 	
     -- Autors  ----------------------------------------------------------------
-    V1.0 - Paul Slats
+    V1.0 - Wizzard72
 	
 	
     -- Uservariables ----------------------------------------------------------
